d3d3cd4ee32442ea0b88be5b37ce086f0e6279d0
hortinstein <hortinstein@hortinstein-VirtualBox.(none)>
2012-12-28 18:28:06 +0900

    fixed test cases

--- test/test.js
+++ test/test.js
@@ -4,28 +4,25 @@ var impurge = require('../impurge.js');
 describe('impurge', function(){
 	describe('image url', function  () {
 		it('should find 1 picture', function(done){
-			impurge.purge("http://i.imgur.com/ndzyY.jpg", function  (e,r) {
-				if (r.length === 1){
-					done();
-				}
+			impurge.purge("http://i.imgur.com/IvpcP.jpg", function  (e,r) {
+				r.length.should.equal(1)
+				done();
 			})    	
     	});
 	});
 	describe('image hash url', function  () {
 		it('should find 1 picture', function(done){
-			impurge.purge("http://imgur.com/Wj8j4", function  (e,r) {
-				if (r.length === 1){
-					done();
-				}
+			impurge.purge("http://imgur.com/wB6Rh", function  (e,r) {
+				r.length.should.equal(1)
+				done();
 			})    	
     	});
 	});
 	describe('album url', function  () {
-		it('should find 18 pictures', function(done){
-			impurge.purge("http://imgur.com/a/mmMXw", function  (e,r) {
-				if (r.length === 18){
-					done();
-				}
+		it('should find 12 pictures', function(done){
+			impurge.purge("http://imgur.com/a/L4gQ7", function  (e,r) {
+				r.length.should.equal(12)
+				done();
 			})    	
     	});
 	});
