38173a4f3913dc5dac4ac9a2c77325c2ae57c96f
Alex Hortin <hortinstein@gmail.com>
2014-06-05 22:25:03 -0400

    removed printing

--- impurge.js
+++ impurge.js
@@ -13,7 +13,6 @@ var imgur_hashes_pattern = RegExp("imgur\.com/(([a-zA-Z0-9]{5,7}[&,]?)+)", "i");
 var imgur_image_pattern = RegExp("^http://(www\.)?(i\.)?imgur\.com/.{3,7}\.((jpg)|(gif)|(png))", "ig");
 
 impurge.get_text_imgur_links = function(text) {
-    console.log(text)
     var imgur_url_pattern = RegExp("(http)(s)?://((m)\.|(www)\.|((i)\.))?imgur.com/(a/)?[a-zA-Z0-9&]+((\.jpg)|(\.gif)|(\.png))?", "igm");
     var matches = text.match(imgur_url_pattern);
     return matches;
